fb8fc35b4ab842c179fd3324ef3720be7898e20a
BrianJenney <brianjenney83@gmail.com>
2018-06-30 13:32:04 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,97 @@
+const fs = require("fs");
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
+const express = require("express");
+const app = express();
+
+app.listen(8000 || process.env.PORT);
+
+app.get("/anagram", (req, res) => {
+    res.send(anagram(req.query.word));
+});
+
+function readFile() {
+    return fs
+        .readFileSync("words.txt")
+        .toString()
+        .split("\n");
+}
+
+const HASH = myCache.get("dict") ? myCache.get("dict") : wordHash();
+
+function wordHash() {
+    let hash = {};
+    const dict = readFile();
+    dict.forEach(word => {
+        if (!hash[wordSorter(word)]) {
+            hash[wordSorter(word)] = word;
+        }
+    });
+    myCache.set("dict", hash, 1000);
+    return hash;
+}
+
+function anagram(word) {
+    let anagrams = [];
+    let twoWords = [];
+
+    const sortedWord = wordSorter(word);
+
+    for (const key of Object.keys(HASH)) {
+        let wordDict = containsWord(sortedWord, key);
+
+        if (wordDict) {
+            anagrams.push(HASH[key]);
+            let secondWord = HASH[wordDict.remainingWord];
+            if (secondWord && !twoWords.length) {
+                twoWords.push(wordDict.comparedWord, secondWord);
+            }
+        }
+    }
+
+    return anagrams.length || twoWords.length
+        ? { anagrams, twoWords }
+        : { message: "Sorry Charlie" };
+}
+
+function containsWord(word, sub) {
+    let curWord = word.split("");
+    let compareWord = sub.split("");
+
+    let match = true;
+
+    while (match) {
+        match = false;
+
+        if (!compareWord.length) {
+            return {
+                comparedWord: HASH[sub],
+                remainingWord: curWord.join("")
+            };
+        }
+
+        if (!curWord.length) {
+            return false;
+        }
+
+        let compIdx = curWord.indexOf(compareWord[0]);
+
+        if (compIdx > -1) {
+            match = true;
+            compareWord.shift();
+            curWord.splice(compIdx, 1);
+        } else {
+            return false;
+        }
+    }
+}
+
+function wordSorter(word) {
+    return word
+        .toLowerCase()
+        .split("")
+        .sort()
+        .join("");
+}
+
+module.exports = app;
---
+++ test/anagram-test.js
@@ -0,0 +1,39 @@
+process.env.NODE_ENV = "test";
+
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("/GET anagram", () => {
+    let tests = [
+        //word          expected
+        ["kissableking", ["bleak", "kissing"]],
+        ["judoking", ["judo", "king"]]
+    ];
+
+    for (let test of tests) {
+        it("it should return an anagram", done => {
+            [word, expected] = test;
+            chai.request(server)
+                .get(`/anagram?word=${word}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.twoWords.should.be.eql(expected);
+                    done();
+                });
+        });
+    }
+
+    it("it should return a not found message", done => {
+        chai.request(server)
+            .get("/anagram?word=asdf")
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.eql({ message: "Sorry Charlie" });
+                done();
+            });
+    });
+});
