88b39a8f9c73d0a01e0539fab59094d90cf6262b
BrianJenney <brianjenney83@gmail.com>
2018-06-30 21:13:30 -0700

    allow cors

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const myCache = new NodeCache();
 const express = require("express");
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 app.listen(process.env.PORT || 8000);
 
 app.get("/anagram", (req, res) => {
